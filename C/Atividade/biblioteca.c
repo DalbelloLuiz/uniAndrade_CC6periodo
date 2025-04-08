54e7ff1ee8b19677f865267fe13bfc7be435fdf4
DalbelloLuiz <143212899+DalbelloLuiz@users.noreply.github.com>
2025-04-08 22:41:21 +0000

    Funcoes de emprestar devolver e menu

--- C/Atividade/biblioteca.c
+++ C/Atividade/biblioteca.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 #define MAX_STR 50
@@ -31,6 +32,7 @@ void cadastrarLivro(struct Livro *livro){
 };
 
 void exibirLivro(struct Livro livro){
+    printf("-------------------------\n");
     printf("Titulo: %s\n", livro.titulo);
     printf("Autor: %s\n", livro.autor);
     printf("Total de emprestimos: %d\n", livro.total_emprestimos);
@@ -42,17 +44,68 @@ void exibirLivro(struct Livro livro){
     }
 }
 
+void emprestarLivro(struct Livro *livro){
+    if(livro->status == EMPRESTADO || livro->total_emprestimos == 3){
+        printf("Lamento o livro nao pode ser emprestado no momento!\n");
+    } else {
+        livro->status = EMPRESTADO;
+        livro->total_emprestimos++;
+        printf("Livro emprestado com sucesso!\n");
+    }
+};
+
+void devolverLivro(struct Livro *livro){
+    if(livro->status == DISPONIVEL){
+        printf("O livro nao se encontra emprestado!\n");
+    }else{
+        livro->status = DISPONIVEL;
+        printf("Livro devolvido com sucesso!\n");
+    }
+}
 
 int main(){
     struct Livro biblioteca[MAX_LIVROS];
     int totalLivros = 0;
-    
-    cadastrarLivro(&biblioteca[totalLivros]);
-    totalLivros++;
-    cadastrarLivro(&biblioteca[totalLivros]);
-    totalLivros++;
-    exibirLivro(biblioteca[0]);
-    exibirLivro(biblioteca[1]);
-    printf("%d\n",totalLivros);
+    int opc;
+
+    do{
+        printf("*****************************\n");
+        printf("**Bibliotecsa Show de BOLA!**\n");
+        printf("*****************************\n");
+        printf("1 - Cadastrar Livro\n");
+        printf("2 - Emprestar Livro\n");
+        printf("3 - Devolver Livro\n");
+        printf("4 - Relatório\n");
+        printf("0 - Sair\n");
+        printf("Escolha uma opcao:");
+        scanf("%d", &opc);
+        switch (opc) {
+        case 1:
+        system("read -p 'Pressione ENTER para continuar...' var");
+        system("clear");
+            break;
+        case 2:
+        system("read -p 'Pressione ENTER para continuar...' var");
+        system("clear");
+            break;
+        case 3:
+        system("read -p 'Pressione ENTER para continuar...' var");
+        system("clear");
+            break;
+        case 4:
+        system("read -p 'Pressione ENTER para continuar...' var");
+        system("clear");
+            break;
+        case 0:
+            printf("Saindo...\n");
+            break;    
+        default:
+            printf("Opcao invalida!\n");
+            system("read -p 'Pressione ENTER para continuar...' var");
+            system("clear");
+            break;
+
+        }
+    } while (opc != 0);
     return 0;
 }

b691ba62253d179a20c8b77661e4c9f34298ce3f
DalbelloLuiz <143212899+DalbelloLuiz@users.noreply.github.com>
2025-04-14 18:42:32 +0000

    Commit alguma coisa

--- C/Atividade/biblioteca.c
+++ C/Atividade/biblioteca.c
@@ -5,25 +5,30 @@
 #define MAX_STR 50
 #define MAX_LIVROS 10
 
-enum StatusLivro {
+enum StatusLivro
+{
     DISPONIVEL,
     EMPRESTADO
 };
 
-struct Livro {
+struct Livro
+{
     char autor[MAX_STR];
     char titulo[MAX_STR];
     int total_emprestimos;
     enum StatusLivro status;
 };
 
-void cadastrarLivro(struct Livro *livro){
+void cadastrarLivro(struct Livro *livro)
+{
     printf("Titulo: ");
-    fflush(stdin);fgets(livro->titulo, MAX_STR, stdin);
+    fflush(stdin);
+    fgets(livro->titulo, MAX_STR, stdin);
     livro->titulo[strcspn(livro->titulo, "\n")] = '\0';
 
     printf("Autor: ");
-    fflush(stdin);fgets(livro->autor, MAX_STR, stdin);
+    fflush(stdin);
+    fgets(livro->autor, MAX_STR, stdin);
     livro->autor[strcspn(livro->autor, "\n")] = '\0';
 
     livro->total_emprestimos = 0;
@@ -31,45 +36,60 @@ void cadastrarLivro(struct Livro *livro){
     printf("Livro castrado com sucesso\n");
 };
 
-void exibirLivro(struct Livro livro[], int totalLivros){
-    for(int i = 0; i < totalLivros ; i++){
+void exibirLivro(struct Livro livro[], int totalLivros)
+{
+    for (int i = 0; i < totalLivros; i++)
+    {
         printf("Livro %d - ", i);
-        printf("Titulo: %s | Autor: %s\n", livro[i].titulo,livro[i].autor);
+        printf("Titulo: %s | Autor: %s\n", livro[i].titulo, livro[i].autor);
         printf("Total de emprestimos: %d\n", livro[i].total_emprestimos);
-        if (livro[i].status == DISPONIVEL){
+        if (livro[i].status == DISPONIVEL)
+        {
             printf("Status: Disponivel\n");
-        } else {
+        }
+        else
+        {
             printf("Status: Emprestado\n");
         }
         printf("------------------------------\n");
     }
 }
 
-void emprestarLivro(struct Livro *livro){
-    if(livro->status == EMPRESTADO || livro->total_emprestimos == 3){
+void emprestarLivro(struct Livro *livro)
+{
+    if (livro->status == EMPRESTADO || livro->total_emprestimos == 3)
+    {
         printf("Lamento o livro nao pode ser emprestado no momento!\n");
-    } else {
+    }
+    else
+    {
         livro->status = EMPRESTADO;
         livro->total_emprestimos++;
         printf("Livro emprestado com sucesso!\n");
     }
 };
 
-void devolverLivro(struct Livro *livro){
-    if(livro->status == DISPONIVEL){
+void devolverLivro(struct Livro *livro)
+{
+    if (livro->status == DISPONIVEL)
+    {
         printf("O livro nao se encontra emprestado!\n");
-    }else{
+    }
+    else
+    {
         livro->status = DISPONIVEL;
         printf("Livro devolvido com sucesso!\n");
     }
 };
 
-int main(){
+int main()
+{
     struct Livro biblioteca[MAX_LIVROS];
     int totalLivros = 0;
     int opc;
 
-    do{
+    do
+    {
         system("clear");
         printf("*****************************\n");
         printf("**Bibliotecsa Show de BOLA!**\n");
@@ -82,71 +102,78 @@ int main(){
         printf("Escolha uma opcao:");
         scanf("%d", &opc);
         getchar();
-        switch (opc) {
+        switch (opc)
+        {
         case 1:
-        system("clear");
-        if (totalLivros >= MAX_LIVROS){
-            printf("Limite de livros atingido!\n");
-            printf("Pressione ENTER para continuar...");
-            getchar();
             system("clear");
-            break;  
-        } else {
-        cadastrarLivro(&biblioteca[totalLivros]);
-        totalLivros++;
-        printf("Pressione ENTER para continuar...");
-        getchar();
-        system("clear");
-            break;
-        }    
+            if (totalLivros >= MAX_LIVROS)
+            {
+                printf("Limite de livros atingido!\n");
+                printf("Pressione ENTER para continuar...");
+                getchar();
+                system("clear");
+                break;
+            }
+            else
+            {
+                cadastrarLivro(&biblioteca[totalLivros]);
+                totalLivros++;
+                printf("Pressione ENTER para continuar...");
+                getchar();
+                system("clear");
+                break;
+            }
         case 2:
-        int aux = 0;
-        system("clear");
-        exibirLivro(biblioteca, totalLivros);
-        printf("Qual livro voce deseja emprestar: ");
-        scanf("%d", &aux);
-        if (aux > totalLivros || aux < 0){
-            printf("Livro inválido!\n");
+            int aux = 0;
+            system("clear");
+            exibirLivro(biblioteca, totalLivros);
+            printf("Qual livro voce deseja emprestar: ");
+            scanf("%d", &aux);
+            if (aux > totalLivros || aux < 0)
+            {
+                printf("Livro inválido!\n");
+                getchar();
+                printf("Pressione ENTER para continuar...");
+                getchar();
+                system("clear");
+                break;
+            }
+            else
+            {
+                emprestarLivro(&biblioteca[aux]);
+                getchar();
+                printf("Pressione ENTER para continuar...");
+                getchar();
+                system("clear");
+                break;
+            }
+        case 3:
+            system("clear");
+            exibirLivro(biblioteca, totalLivros);
+            printf("Qual livro voce deseja emprestar: ");
+            scanf("%d", &aux);
+            devolverLivro(&biblioteca[aux]);
             getchar();
             printf("Pressione ENTER para continuar...");
             getchar();
             system("clear");
-            break;            
-        } else{ emprestarLivro(&biblioteca[aux]);
-        getchar();
-        printf("Pressione ENTER para continuar...");
-        getchar();
-        system("clear");
-            break;
-        }    
-        case 3:
-        system("clear");
-        exibirLivro(biblioteca, totalLivros);
-        printf("Qual livro voce deseja emprestar: ");
-        scanf("%d", &aux);
-        devolverLivro(&biblioteca[aux]);
-        getchar();
-        printf("Pressione ENTER para continuar...");
-        getchar();
-        system("clear");
             break;
         case 4:
-        system("clear");
-        exibirLivro(biblioteca, totalLivros);
-        printf("Pressione ENTER para continuar...");
-        getchar();
-        system("clear");
+            system("clear");
+            exibirLivro(biblioteca, totalLivros);
+            printf("Pressione ENTER para continuar...");
+            getchar();
+            system("clear");
             break;
         case 0:
             printf("Saindo...\n");
-            break;    
+            break;
         default:
             printf("Opcao invalida!\n");
             printf("Pressione ENTER para continuar...");
             getchar();
             system("clear");
             break;
-
         }
     } while (opc != 0);
     return 0;
